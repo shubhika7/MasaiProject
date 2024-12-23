86f4bc58d1845da3417881cc96fa4d4ff276df0f
shubhika7 <108242980+shubhika7@users.noreply.github.com>
2024-12-23 20:50:03 +0530

    Add files via upload

---
+++ class1.py
@@ -0,0 +1,29 @@
+a = 1 #integer
+b = 11.1 #float
+# a-> variable name
+# 10-> value
+e = 1.2 #float
+e = "hello"
+print(type(a))
+print(type(b))
+print(type(e))
+e= True #boolean
+e=bool("")
+print(e)
+e=bool(100)
+
+print(e)
+d= True or False
+d=int(True)
+print(type(d))
+#Variables
+#rule1-> alphabets, numbers, underscore
+#rule2-> cannot start with number
+#rule3-> not whitespaces
+#rule4-> can't use keywords
+a = int(input("Enter the name: "))
+b = int(input("Enter second number: "))
+print("sum is: ", a+b)
+
+
+
